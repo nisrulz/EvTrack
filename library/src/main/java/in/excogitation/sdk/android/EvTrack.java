57acb7190b6e3c4bd02ff4e5bbb15d1a6cbfe575
Nishant Srivastava <nisrulz@gmail.com>
2016-05-28 17:58:11 +0530

    [*] updated codebase and dependencies

--- library/src/main/java/in/excogitation/sdk/android/EvTrack.java
+++ library/src/main/java/in/excogitation/sdk/android/EvTrack.java
@@ -20,128 +20,120 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.support.v4.util.ArrayMap;
 import android.util.Log;
-
-import com.squareup.okhttp.Call;
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.FormEncodingBuilder;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-
-import in.excogitation.lib_deviceinfo.EasyDeviceInfo;
+import github.nisrulz.easydeviceinfo.EasyDeviceInfo;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class EvTrack {
-    private EasyDeviceInfo deviceInfo;
-    private OkHttpClient client;
-    private final String LOGTAG = getClass().getSimpleName().toString();
+  private EasyDeviceInfo deviceInfo;
+  private OkHttpClient client;
+  private final String LOGTAG = getClass().getSimpleName().toString();
 
-    private Context context;
+  private Context context;
 
-    private boolean enableLogs_exp = false;
-    private boolean enableLogs_event = false;
+  private boolean enableLogs_exp = false;
+  private boolean enableLogs_event = false;
 
-    public EvTrack(Context context) {
-        this.context = context;
+  public EvTrack(Context context) {
+    this.context = context;
+  }
+
+  public void enableLogs() {
+    enableLogs_event = true;
+    enableLogs_exp = true;
+  }
+
+  public void recordEvent(ArrayMap<String, String> event_params, String url,
+      Callback httpCallback) {
+
+    //Initialize DeviceInfo
+    deviceInfo = new EasyDeviceInfo(context);
+    //Initialize Http Client
+    client = new OkHttpClient();
+
+    //Log the Event details
+    if (enableLogs_event) {
+      Log.d(LOGTAG, "*-------------Event-------------*\n");
+      for (int i = 0; i < event_params.size(); i++) {
+        Log.d(LOGTAG, "\nKey : " + event_params.keyAt(i) + "\tValue : " + event_params.valueAt(i));
+      }
     }
 
-    public void enableLogs() {
-        enableLogs_event = true;
-        enableLogs_exp = true;
+    //Get LatLong
+    double[] latlong = deviceInfo.getLatLong();
+
+    //Push extra data in event request
+    event_params.put("aid", deviceInfo.getAndroidID());
+    event_params.put("act", deviceInfo.getActivityName());
+    event_params.put("time", String.valueOf(deviceInfo.getTime()));
+    event_params.put("ct", deviceInfo.getNetworkType());
+    event_params.put("lat", String.valueOf(latlong[0]));
+    event_params.put("lon", String.valueOf(latlong[1]));
+
+    if (context.checkCallingOrSelfPermission(android.Manifest.permission.INTERNET)
+        == PackageManager.PERMISSION_GRANTED) {
+      postReq(client, url, event_params, httpCallback);
+    } else {
+      Log.e(LOGTAG, "INTERNET permission not granted! No request was made to the server!");
+    }
+  }
+
+  public void recordException(Exception exp, ArrayMap<String, String> exp_params, String url,
+      Callback httpCallback) {
+
+    deviceInfo = new EasyDeviceInfo(context);
+    //Initialize Http Client
+    client = new OkHttpClient();
+
+    exp_params.put("etype", exp.getClass().getName());
+    exp_params.put("ecause", exp.getCause().toString());
+    exp_params.put("emsg", exp.getMessage());
+    exp_params.put("time", Long.toString(deviceInfo.getTime()));
+    exp_params.put("aid", deviceInfo.getAndroidID());
+    exp_params.put("mk", deviceInfo.getManufacturer());
+    exp_params.put("mo", deviceInfo.getModel());
+    exp_params.put("osv", deviceInfo.getOSVersion());
+
+    //Log the Exception details
+    if (enableLogs_exp) {
+      Log.e(LOGTAG, "*------SDK Encountered an exception !!------*\n");
+      Log.e(LOGTAG, "Exception : " + exp_params.get("etype"));
+      Log.e(LOGTAG, "Cause : " + exp_params.get("ecause"));
+      Log.e(LOGTAG, "Msg : " + exp_params.get("emsg"));
+      Log.e(LOGTAG, "Device : " + exp_params.get("mk") + "_" + exp_params.get("mo"));
     }
 
-    public void recordEvent(ArrayMap<String, String> event_params, String url, Callback httpCallback) {
+    if (context.checkCallingOrSelfPermission(android.Manifest.permission.INTERNET)
+        == PackageManager.PERMISSION_GRANTED) {
+      postReq(client, url, exp_params, httpCallback);
+    } else {
+      Log.e(LOGTAG, "INTERNET permission not granted! No request was made to the server!");
+    }
+  }
 
-        //Initialize DeviceInfo
-        deviceInfo = new EasyDeviceInfo(context);
-        //Initialize Http Client
-        client = new OkHttpClient();
+  /**
+   * Make a POST Request
+   */
+  private void postReq(OkHttpClient client, String url, ArrayMap<String, String> params,
+      Callback callback) {
 
-        //Log the Event details
-        if (enableLogs_event) {
-            Log.d(LOGTAG, "*-------------Event-------------*\n");
-            for (int i = 0; i < event_params.size(); i++) {
-                Log.d(LOGTAG, "\nKey : " + event_params.keyAt(i) + "\tValue : " + event_params.valueAt(i));
-            }
-        }
-
-        //Get LatLong
-        double[] latlong = deviceInfo.getLatLong();
-
-        //Push extra data in event request
-        event_params.put("aid", deviceInfo.getAndroidID());
-        event_params.put("act", deviceInfo.getActivityName());
-        event_params.put("time", String.valueOf(deviceInfo.getTime()));
-        event_params.put("ct", deviceInfo.getNetworkType());
-        event_params.put("lat", String.valueOf(latlong[0]));
-        event_params.put("lon", String.valueOf(latlong[1]));
-
-        if (context.checkCallingOrSelfPermission(android.Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
-            postReq(client, url, event_params, httpCallback);
-        } else {
-            Log.e(LOGTAG, "INTERNET permission not granted! No request was made to the server!");
-        }
-
+    FormBody.Builder formdata = new FormBody.Builder();
+    for (int i = 0; i < params.size(); i++) {
+      formdata.add(params.keyAt(i), params.valueAt(i));
     }
 
-    public void recordException(Exception exp, ArrayMap<String, String> exp_params, String url, Callback httpCallback) {
+    RequestBody formBody = formdata.build();
 
-        deviceInfo = new EasyDeviceInfo(context);
-        //Initialize Http Client
-        client = new OkHttpClient();
-
-        exp_params.put("etype", exp.getClass().getName());
-        exp_params.put("ecause", exp.getCause().toString());
-        exp_params.put("emsg", exp.getMessage());
-        exp_params.put("time", Long.toString(deviceInfo.getTime()));
-        exp_params.put("aid", deviceInfo.getAndroidID());
-        exp_params.put("mk", deviceInfo.getManufacturer());
-        exp_params.put("mo", deviceInfo.getModel());
-        exp_params.put("osv", deviceInfo.getOSVersion());
-
-        //Log the Exception details
-        if (enableLogs_exp) {
-            Log.e(LOGTAG, "*------SDK Encountered an exception !!------*\n");
-            Log.e(LOGTAG, "Exception : " + exp_params.get("etype"));
-            Log.e(LOGTAG, "Cause : " + exp_params.get("ecause"));
-            Log.e(LOGTAG, "Msg : " + exp_params.get("emsg"));
-            Log.e(LOGTAG, "Device : " + exp_params.get("mk") + "_"
-                    + exp_params.get("mo"));
-        }
-
-        if (context.checkCallingOrSelfPermission(android.Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
-            postReq(client, url, exp_params, httpCallback);
-        } else {
-            Log.e(LOGTAG, "INTERNET permission not granted! No request was made to the server!");
-        }
-
+    Request request = new Request.Builder().url(url).post(formBody).build();
+    try {
+      Call call = client.newCall(request);
+      call.enqueue(callback);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-
-    /**
-     * Make a POST Request
-     *
-     * @param client
-     * @param url
-     * @param params
-     * @param callback
-     */
-    private void postReq(OkHttpClient client, String url, ArrayMap<String, String> params, Callback callback) {
-        FormEncodingBuilder formdata = new FormEncodingBuilder();
-        for (int i = 0; i < params.size(); i++) {
-            formdata.add(params.keyAt(i), params.valueAt(i));
-        }
-
-        RequestBody formBody = formdata.build();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .post(formBody)
-                .build();
-        try {
-            Call call = client.newCall(request);
-            call.enqueue(callback);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
+  }
 }
--- sample/src/main/java/in/excogitation/evtrack/MainActivity.java
+++ sample/src/main/java/in/excogitation/evtrack/MainActivity.java
@@ -22,67 +22,58 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-
-import java.io.IOException;
-
 import in.excogitation.sdk.android.EvTrack;
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EvTrack evTrack;
-    private final String SERVER = "http://xx.xxx.xxx.xx:xxxx/";
-    private final String LOGTAG = getClass().getSimpleName().toString();
+  private EvTrack evTrack;
+  private final String SERVER = "http://xx.xxx.xxx.xx:xxxx/";
+  private final String LOGTAG = getClass().getSimpleName().toString();
 
-    Button btn_event, btn_exp;
+  Button btn_event, btn_exp;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        evTrack = new EvTrack(getApplicationContext());
-        evTrack.enableLogs();
+    evTrack = new EvTrack(getApplicationContext());
+    evTrack.enableLogs();
 
-        btn_event = (Button) findViewById(R.id.button_event);
-        btn_event.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ArrayMap<String, String> event_params = new ArrayMap<String, String>();
-                event_params.put("sdk", "1.0");
-                evTrack.recordEvent(event_params, SERVER + "test", httpCallback);
-            }
-        });
+    btn_event = (Button) findViewById(R.id.button_event);
+    btn_event.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        ArrayMap<String, String> event_params = new ArrayMap<String, String>();
+        event_params.put("sdk", "1.0");
+        evTrack.recordEvent(event_params, SERVER + "test", httpCallback);
+      }
+    });
 
+    btn_exp = (Button) findViewById(R.id.button_exp);
+    btn_exp.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        try {
+          throw new RuntimeException("This is an Error");
+        } catch (RuntimeException e) {
+          ArrayMap<String, String> exp_param = new ArrayMap<String, String>();
+          exp_param.put("code", "SP0002");
+          exp_param.put("sdk", "1.1");
+          evTrack.recordException(e, exp_param, SERVER + "test", httpCallback);
+        }
+      }
+    });
+  }
 
-        btn_exp = (Button) findViewById(R.id.button_exp);
-        btn_exp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    throw new RuntimeException("This is an Error");
-                } catch (RuntimeException e) {
-                    ArrayMap<String, String> exp_param = new ArrayMap<String, String>();
-                    exp_param.put("code", "SP0002");
-                    exp_param.put("sdk", "1.1");
-                    evTrack.recordException(e, exp_param, SERVER + "test", httpCallback);
-                }
-            }
-        });
+  private final Callback httpCallback = new Callback() {
+    @Override public void onFailure(Call call, IOException e) {
+      Log.e(LOGTAG, "Req Failed : " + e.getLocalizedMessage());
     }
 
-    private final Callback httpCallback = new Callback() {
-        @Override
-        public void onFailure(Request request, IOException e) {
-            Log.e(LOGTAG, "Req Failed : " + e.getLocalizedMessage());
-        }
-
-        @Override
-        public void onResponse(Response response) throws IOException {
-            Log.i(LOGTAG, "Req Successful : " + String.valueOf(response.code()));
-        }
-    };
+    @Override public void onResponse(Call call, Response response) throws IOException {
+      Log.i(LOGTAG, "Req Successful : " + String.valueOf(response.code()));
+    }
+  };
 }
